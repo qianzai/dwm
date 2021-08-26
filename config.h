f38be94c10ac9b16601ce0708be64f422148944b
qianzai <mail@bzm.ink>
2021-08-26 18:06:12 +0800

    google-chrome

--- config.h
+++ config.h
@@ -84,7 +84,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *chromecmd[]  = { "google-chrome-stable", NULL };
+static const char *chromecmd[]  = { "google-chrome-stable","--force-device-scale-factor=1.15",NULL };
 static const char *ideacmd[]  = { "/usr/local/bin/idea", NULL };
 static const char *typoracmd[]  = { "typora", NULL };
 static const char *utoolscmd[]  = { "utools", NULL };
